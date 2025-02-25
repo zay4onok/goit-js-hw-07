3c7c4f1212005f12833e2bb4b77105a84ad4d6d4
Dmitry <dmitronus2015@gmail.com>
2025-02-25 20:11:04 +0200

    add second homework

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(e) {
+  e.preventDefault();
+  if (e.target.tagName !== "IMG") {
+    return;
+  }
+}
+
+function galleryItemsTemplate(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+    `;
+    })
+    .join("");
+}
+
+function renderGalleryItems(galleryItems) {
+  gallery.innerHTML = galleryItemsTemplate(galleryItems);
+}
+
+renderGalleryItems(galleryItems);
+
+let newGallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
